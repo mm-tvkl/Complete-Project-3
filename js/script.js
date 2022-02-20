e7ad52af403b97f993d2cc9f3f94f535ea96d704
mm-tvkl <mohammadmahdi.tavakoli.1384@gmail.com>
2022-02-20 12:01:35 +0330

    Screenshot Section added

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("#owl-carousel1").owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                loop: true
+            },
+            600: {
+                items: 3,
+                nav: false,
+                loop: true
+            },
+            1000: {
+                items: 4,
+                nav: false,
+                loop: true
+            }
+        },
+        URLhashListener: true,
+        startPosition: 'zero',
+        stagePadding: 50,
+        autoplay: true,
+        autoplay: 2000
+    });
+});
