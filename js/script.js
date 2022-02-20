022095fd845b8fe86441da424f9bcc7d6e9d70f6
mm-tvkl <mohammadmahdi.tavakoli.1384@gmail.com>
2022-02-20 21:42:45 +0330

    Footer Section and Author Name

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $("#owl-carousel1").owlCarousel({
         loop: true,
         margin: 20,
         responsiveClass: true,
-        autoplayHoverPause:true,
+        autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1,
@@ -15,10 +15,10 @@ $(document).ready(function () {
                 nav: false,
                 loop: true
             },
-            900:{
-                items:2,
-                nav:false,
-                loop:true
+            900: {
+                items: 2,
+                nav: false,
+                loop: true
             },
             1000: {
                 items: 4,
@@ -31,33 +31,38 @@ $(document).ready(function () {
         stagePadding: 50,
         autoplay: true,
         autoplay: 2000,
-        dotsEach:2,
-        dotsSpeed:200
+        dotsEach: 2,
+        dotsSpeed: 200
     });
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     $("#owl-carousel2").owlCarousel({
         loop: true,
         margin: 20,
         responsiveClass: true,
-        autoplayHoverPause:true,
+        autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1,
                 nav: false,
                 loop: true
             },
-            991:{
-                items:2,
-                nav:false,
-                loop:true
+            991: {
+                items: 2,
+                nav: false,
+                loop: true
             }
         },
         stagePadding: 50,
         autoplay: true,
         autoplay: 2000,
-        dotsEach:1,
-        dotsSpeed:200
+        dotsEach: 1,
+        dotsSpeed: 200
     });
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
