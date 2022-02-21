80d73e9470bc09d311a96191afe68a6d98bb69a9
mm-tvkl <mohammadmahdi.tavakoli.1384@gmail.com>
2022-02-21 18:24:41 +0330

    Bugs Fixed

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#owl-carousel1").owlCarousel({
         loop: true,
         margin: 20,
         responsiveClass: true,
         autoplayHoverPause: true,
+
         responsive: {
             0: {
                 items: 1,
                 nav: false,
-                loop: true
+                loop: true,
+                stagePadding: 30,
+                dotsEach: 1
             },
             600: {
                 items: 3,
                 nav: false,
-                loop: true
+                loop: true,
+                stagePadding: 50,
+                dotsEach: 1
             },
             900: {
                 items: 2,
                 nav: false,
-                loop: true
+                loop: true,
+                stagePadding: 50,
+                dotsEach: 1
             },
             1000: {
                 items: 4,
                 nav: false,
-                loop: true
+                loop: true,
+                stagePadding: 30,
+                dotsEach: 2
             }
         },
         URLhashListener: true,
         startPosition: "zero",
-        stagePadding: 50,
         autoplay: true,
         autoplay: 2000,
         dotsEach: 2,
@@ -36,7 +44,7 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#owl-carousel2").owlCarousel({
         loop: true,
         margin: 20,
@@ -46,18 +54,21 @@ $(document).ready(function() {
             0: {
                 items: 1,
                 nav: false,
-                loop: true
+                loop: true,
+                stagePadding: 30,
+                dotsEach: 1
             },
             991: {
                 items: 2,
                 nav: false,
-                loop: true
+                loop: true,
+                stagePadding: 50,
+                dotsEach: 1
             }
         },
         stagePadding: 50,
         autoplay: true,
         autoplay: 2000,
-        dotsEach: 1,
         dotsSpeed: 200
     });
 });
@@ -65,12 +76,12 @@ $(document).ready(function() {
 var tooltipTriggerList = [].slice.call(
     document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
-var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
 var navbar = document.getElementById("Navbar");
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var y = window.scrollY;
     if (y >= 200) {
         navbar.classList.add("navbar-bg-purple");
